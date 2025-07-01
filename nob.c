323e29697cf44ee6ab416ce30f941ad7d7be89aa
constantijn <cdemeer@fim.uni-passau.de>
2025-07-01 17:04:49 +0200

    nob: multiple target building, update todo

--- nob.c
+++ nob.c
@@ -7,14 +7,13 @@
 
 void print_usage(const char *program_name)
 {
-    printf("Usage: %s <target>\n\n", program_name);
+    printf("Usage: %s <targets..>\n\n", program_name);
 
     printf("Targets:\n");
     printf("  - cli        Build the cli\n");
     printf("  - gui        Build the gui\n");
     printf("  - lib        Build the core functionality lib\n");
-    printf("  - all        Build the all the above\n");
-    printf("  - test       Build the current test\n\n");
+    printf("  - all        Build all the above\n\n");
     
     printf("Other:\n");
     printf("  - help       Print this dialog\n\n");
@@ -58,13 +57,6 @@ bool build_cli(Nob_Cmd *cmd)
 bool build_gui(Nob_Cmd *cmd)
 {
     nob_log(NOB_INFO, "Building gui..");
-    const char *raylib_path = "lib/libraylib.a";
-    struct stat attr;
-    if (stat("lib/libraylib.a", &attr) == -1){
-        nob_log(NOB_ERROR, "Raylib static library not found at at %s.", raylib_path);
-        nob_log(NOB_ERROR, "Please provide a built raylib lib in that location to procede.");
-        return false;
-    }
     append_head(cmd);
     nob_cmd_append(cmd, "-o", "build/gui");
     nob_cmd_append(cmd, "src/gui.c", "-Lbuild", "-lcore", "-Llib", "-lraylib", "-lgdi32", "-lwinmm");
@@ -73,19 +65,11 @@ bool build_gui(Nob_Cmd *cmd)
 #ifndef _WIN32
     nob_cmd_append(cmd, "-Wl,-rpath,build");
 #endif // _WIN32
-    return nob_cmd_run_sync_and_reset(cmd);
-}
-
-bool build_test(Nob_Cmd *cmd)
-{
-    nob_log(NOB_INFO, "Building test..");
-    append_head(cmd);
-    nob_cmd_append(cmd, "-o", "build/test");
-    nob_cmd_append(cmd, "testing/thread_test.c", "-Lbuild", "-lcore");
-#ifndef _WIN32
-    nob_cmd_append(cmd, "-Wl,-rpath,build");
-#endif // _WIN32
-    return nob_cmd_run_sync_and_reset(cmd);
+    if (!nob_cmd_run_sync_and_reset(cmd)){
+        nob_log(NOB_ERROR, "Failed to build gui! Make sure that you have raylib in your path or in ./lib!");
+        return false;
+    }
+    return true;
 }
 
 bool build_all(Nob_Cmd *cmd)
@@ -96,14 +80,19 @@ bool build_all(Nob_Cmd *cmd)
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
+    Nob_Cmd cmd = {0};
+    const char *program_name = nob_shift_args(&argc, &argv);
+    if (argc == 0){
+        fprintf(stderr, "[ERROR] No target provided!\n");
+        print_usage(program_name);
+        return 1;
+    }
     if (!nob_mkdir_if_not_exists("build")){
         nob_log(NOB_ERROR, "Could not create build directory!");
         return 1;
     }
-    Nob_Cmd cmd = {0};
-    const char *program_name = argv[0];
-    if (argc > 1){
-        const char *target = argv[1];
+    while (argc > 0){
+        const char *target = nob_shift_args(&argc, &argv);
         if (strcmp(target, "cli") ==  0) {
             if (!build_cli(&cmd)) return 1;
         }
@@ -116,16 +105,14 @@ int main(int argc, char **argv)
         else if (strcmp(target, "all") == 0) {
             if (!build_all(&cmd)) return 1;
         }
-        else if (strcmp(target, "test") == 0) {
-            if (!build_test(&cmd)) return 1;
+        else if (strcmp(target, "help") == 0){
+            print_usage(program_name);
+            return 0;
         }
-        else if (strcmp(target, "help") == 0) print_usage(program_name);
         else{
             fprintf(stderr, "[ERROR] Unknown target: '%s'!\n", target);
             return 1;
         }
-    } else{
-        build_all(&cmd);
     }
     return 0;
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,13 @@
 # Todo:
 
-- Fix %u issues for Windows and Linux
-- do testing
-- write gui client
+- Linux testing
+- fix path sep spam on windows
+- lib:
+    - add more logging (maybe controllable through log level?)
+- nob: 
+    - allow static and dynamic linking with raylib?
+- gui: 
+    - improve log scrolling
+    - clean up unused ui
+    - add way to see backups of branch without clicking on "Make Backup"
+    - enable colored log
